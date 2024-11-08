11b9ea70a96134c999859961699c17ce709c3b71
sintakkin@yahoo.com.hk <sintakkin@yahoo.com.hk>
2024-11-08 15:34:22 +0800

    Init

---
+++ querystring.js
@@ -0,0 +1,93 @@
+class QueryString {
+	constructor(){
+		this.refresh();
+	}
+	refresh(){
+		this.href = window.location.href.toString();
+		this.getKeyVals();
+	}
+	// get key value array
+	getKeyVals(){
+		let tmp = this.href.split('?');
+		let results = [];
+
+		if(tmp.length > 1){
+			// has query string
+			let arrQs = tmp[1].split('&');
+			for(let qs in arrQs){
+				let keyVal = arrQs[qs].split('=');
+				results.push({
+					key: keyVal[0].toLowerCase(),
+					val: keyVal[1]
+				});
+			}
+		}
+
+		this.keyVals = results;
+		return results;
+	}
+	// get keys
+	getKeys(){
+		let keys = [];
+		for(let k in this.keyVals){
+			keys.push(this.keyVals[k].key);
+		}
+		return keys;
+	}
+	// exists key
+	exists(key){
+		return (this.keyVals.find(r => r.key == key.toLowerCase()) != undefined);
+	}
+	// get value
+	get(key){
+		let nKey = key.toLowerCase();
+		let result = null;
+
+		// get value
+		if(this.exists(nKey)){
+			let qs = this.keyVals.find(r => r.key == nKey);
+			if(qs)
+				result = qs.val;
+		}
+
+		return result;
+	}
+	// set value
+	set(key, val){
+		let nKey = key.toLowerCase();
+		// set value
+		if(!this.exists(nKey)){
+			// add
+			this.keyVals.push({
+				'key': nKey,
+				'val': val
+			});
+		}
+		else{
+			// update
+			let idx = this.keyVals.findIndex(r=>r.key == nKey);
+			this.keyVals[idx].val = val;
+		}
+	}
+	// generate href
+	getHref(){
+		if(this.keyVals.length > 0){
+			let url = this.href.split('?')[0]
+
+			for(let i = 0; i<this.keyVals.length; i++){
+				let qs = this.keyVals[i];
+				let sep = (i === 0) ? '?' : '&';
+
+				url += sep + qs.key + '=' + qs.val;
+			}
+
+			this.href = url;
+		}
+
+		return this.href;
+	}
+	// toJson
+	toJsonString(){
+		return JSON.stringify(this.keyVals);
+	}
+}
